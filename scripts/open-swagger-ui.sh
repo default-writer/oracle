21e0a5f96645dd02948275c534a01458903b5021
funcelotwork <artur.mustafin@gmail.com>
2023-10-06 05:11:05 +0000

    added swagger ui

--- scripts/open-swagger-ui.sh
+++ scripts/open-swagger-ui.sh
@@ -13,13 +13,13 @@ PWD=$(pwd)
 
 # run swagger-editor container with the yaml, if not running yet
 # opening specific file doesn't work with editor
-name='swagger-editor'
+name='swagger-ui'
 command -v docker >/dev/null 2>&1 || {
 	echo >&2 "'docker' is not install installed. Aborting."
 	exit 1
 }
 
 # trunk-ignore(shellcheck/SC2312)
-[[ "$(docker ps -f "name=${name}" --format '{{.Names}}')" == "${name}" ]] || docker run --rm -d -p 8044:8080 --name "${name}" -e SWAGGER_JSON=/config/openapi.yaml -v "${PWD}"/config:/config swaggerapi/swagger-editor
+[[ "$(docker ps -f "name=${name}" --format '{{.Names}}')" == "${name}" ]] || docker run --rm -d -p 8045:8080 --name "${name}" -e SWAGGER_JSON=/config/openapi.yml -v "${PWD}"/config:/config swaggerapi/swagger-ui
 
 wait_container_to_be_running "${name}" & sleep 2
